52ca9a3c5eeabf76fc315d0c5046f75524be0248
Derek Tran <dtran110@my.bcit.ca>
2023-06-04 16:51:46 -0700

    Define find_open_space function with doctests

---
+++ connect_four/player_choice/find_open_space.py
@@ -0,0 +1,48 @@
+def find_open_space(board: dict, chosen_column: int, rows: int) -> tuple:
+    """
+    Find an open space on the board in the desired column.
+
+    :param board: a dictionary representing the game board
+    :param chosen_column: an integer representing the desired column on the game board
+    :param rows: an integer representing the number of rows on the game board
+    :precondition: board must be a dictionary
+    :precondition: chosen_column must be an integer
+    :precondition: rows must be an integer
+    :precondition: rows must be positive
+    :postcondition: finds first open space on the board in the desired column
+    :return: a tuple that represents the first open space on the board in the desired column
+    :raises TypeError: if board is not a dictionary
+    :raises TypeError: if chosen_column is not an integer
+    :raises TypeError: if rows is not an integer
+    :raises ValueError: if rows is not positive
+    >>> test_board_open_at_bottom = {(1, 1): '[ ]', (1, 2): '[ ]', (2, 1): '[ ]', (2, 2): '[ ]'}
+    >>> find_open_space(test_board_open_at_bottom, 2, 2)
+    (2, 2)
+    >>> test_board_open_at_top = {(1, 1): '[ ]', (1, 2): '[ ]', (2, 1): '[O]', (2, 2): '[ ]'}
+    >>> find_open_space(test_board_open_at_top, 1, 2)
+    (1, 1)
+    >>> test_board_open_middle = {(1, 1): '[ ]', (1, 2): '[O]', (1, 3): '[ ]', (2, 1): '[O]', (2, 2): '[O]',
+    ... (2, 3): '[ ]', (3, 1): '[ ]', (3, 2): '[O]', (3, 3): '[O]'}
+    >>> find_open_space(test_board_open_middle, 3, 3)
+    (2, 3)
+    """
+    if type(board) is not dict:
+        raise TypeError("The board must be represented with a dictionary.")
+    if type(chosen_column) is not int or type(rows) is not int:
+        raise TypeError("The chosen column and number of rows must both be integers")
+    if rows < 0:
+        raise ValueError("The number of rows must be positive")
+    for row in sorted(range(1, rows + 1), reverse=True):
+        if board.get((row, chosen_column)) == "[ ]":
+            return row, chosen_column
+
+
+def main():
+    """
+    Drive the program.
+    """
+    print("Welcome to the module")
+
+
+if __name__ == "__main__":
+    main()
