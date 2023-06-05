652d534ad27e79e212b22b7aee75b2ed5309c826
Derek Tran <dtran110@my.bcit.ca>
2023-06-04 22:48:45 -0700

    Define check_diagonal_win_down function

---
+++ connect_four/win_conditions/check_diagonal_win_down.py
@@ -0,0 +1,61 @@
+def check_diagonal_win_down(board: dict, row: int, column: int) -> bool:
+    """
+    Check if the player has connected four pieces diagonally downward.
+
+    :param board: a dictionary representing the game board
+    :param row: an integer representing the row number being examined on the game board
+    :param column: an integer representing the column number being examined on the game board
+    :precondition: board must be a dictionary
+    :precondition: row must be an integer
+    :precondition: column must be an integer
+    :postcondition: checks if four adjacent spaces in a diagonal on the game board are all the same and not the default
+    :return: True if four adjacent spaces in a diagonal on the game board are the same and not the default, else false
+    :raises TypeError: if board is not a dictionary
+    :raises TypeError: if row is not an integer
+    :raises TypeError: if column is not an integer
+    >>> test_board_win = {(1, 1): '[O]', (1, 2): '[ ]', (1, 3): '[ ]', (1, 4): '[ ]', (2, 1): '[ ]',
+    ... (2, 2): '[O]', (2, 3): '[ ]', (2, 4): '[ ]', (3, 1): '[ ]', (3, 2): '[ ]', (3, 3): '[O]', (3, 4):
+    ... '[ ]', (4, 1): '[O]', (4, 2): '[ ]', (4, 3): '[ ]', (4, 4): '[O]'}
+    >>> check_diagonal_win_down(test_board_win, 2, 2)
+    True
+    >>> test_board_no_win = {(1, 1): '[O]', (1, 2): '[ ]', (1, 3): '[ ]', (1, 4): '[ ]', (2, 1): '[ ]',
+    ... (2, 2): '[ ]', (2, 3): '[ ]', (2, 4): '[ ]', (3, 1): '[ ]', (3, 2): '[ ]', (3, 3): '[ ]', (3, 4):
+    ... '[ ]', (4, 1): '[O]', (4, 2): '[ ]', (4, 3): '[ ]', (4, 4): '[O]'}
+    >>> check_diagonal_win_down(test_board_no_win, 2, 2)
+    False
+    >>> test_board_win_empty_top_row = {(1, 1): '[ ]', (1, 2): '[ ]', (1, 3): '[ ]', (1, 4): '[ ]', (2, 1): '[O]',
+    ... (2, 2): '[ ]', (2, 3): '[ ]', (2, 4): '[ ]', (3, 1): '[ ]', (3, 2): '[O]', (3, 3): '[ ]', (3, 4):
+    ... '[ ]', (4, 1): '[O]', (4, 2): '[ ]', (4, 3): '[O]', (4, 4): '[ ]', (5, 1): '[ ]', (5, 2): '[ ]',
+    ... (5, 3): '[ ]', (5, 4): '[O]'}
+    >>> check_diagonal_win_down(test_board_win_empty_top_row, 5, 4)
+    True
+    """
+    if type(board) is not dict:
+        raise TypeError("The board must be represented with a dictionary.")
+    if type(row) is not int or type(column) is not int:
+        raise TypeError("The chosen column and number of columns must both be integers")
+    current_symbol = "[ ]"
+    increment = 6
+    while increment > -7:
+        increment -= 1
+        next_symbol = board.get((row - increment, column - increment))
+        if (row - increment, column - increment) in board.keys():
+            if next_symbol != current_symbol:
+                current_symbol = next_symbol
+                counter = 1
+            elif next_symbol == current_symbol and next_symbol is not None and next_symbol != "[ ]":
+                counter += 1
+                if counter == 4:
+                    return True
+    return False
+
+
+def main():
+    """
+    Drive the program.
+    """
+    print("Welcome to the module")
+
+
+if __name__ == "__main__":
+    main()
