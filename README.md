6c0c8712cacbdfbe010555b64f644858d273e953
Derek Tran <dtran110@my.bcit.ca>
2023-06-05 17:10:57 -0700

    Create uncolored version of connectfour

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 - Have Python installed
 - Go to the directory where these files are located from your terminal
 - enter "py [game_name].py" into the command line, where [game_name] is replaced with the name of the file containing the game
+  - If you see incomprehensible strings of numbers and letters, try using the uncolored version of the game by entering py [game_name]_uncolored.py after exiting the game
 - Get a friend
 - Play the game using your keyboard
+- exit the game from terminal with Ctrl + C
---
+++ connectfour_uncolored.py
@@ -0,0 +1,55 @@
+from itertools import cycle
+from common_features import check_board_full, get_player_choice, make_board, show_board, show_column_headers
+from connect_four.player_choice import check_column_full, find_open_space
+from connect_four.win_conditions import check_vertical_win, check_horizontal_win, check_diagonal_win
+
+
+def connect_four():
+    """
+    Play connect four in terminal.
+    """
+    player_one = {"name": "Player 1 (x)", "symbol": "[x]"}
+    player_two = {"name": "Player 2 (o)", "symbol": "[o]"}
+    player_loop = cycle((player_one, player_two))
+    valid_choices = ("1", "2", "3", "4", "5", "6", "7")
+    board = make_board.make_board_numbered(7, 7)
+    show_column_headers.show_column_headers(7)
+    show_board.show_board(board, 7, 7)
+    game_finished = False
+    while not game_finished:
+        current_player = next(player_loop)
+        print(f"\n{current_player['name']}, it's your turn!")
+        chosen_column = int(get_player_choice.get_player_choice
+                            (valid_choices, "What column do you want to drop your token?"))
+        if not check_column_full.check_column_full(board, chosen_column, 7):
+            chosen_row = find_open_space.find_open_space(board, chosen_column, 7)[0]
+            board[(chosen_row, chosen_column)] = current_player["symbol"]
+            show_column_headers.show_column_headers(7)
+            show_board.show_board(board, 7, 7)
+
+            game_finished = check_board_full.check_board_full(board, "[ ]") or \
+                check_vertical_win.check_vertical_win(board, chosen_column, 7) or \
+                check_horizontal_win.check_horizontal_win(board, chosen_row, 7) or \
+                check_diagonal_win.check_diagonal_win_up(board, chosen_row, chosen_column) or \
+                check_diagonal_win.check_diagonal_win_down(board, chosen_row, chosen_column)
+        else:
+            print("That column is full!")
+            show_column_headers.show_column_headers(7)
+            show_board.show_board(board, 7, 7)
+            next(player_loop)
+    if check_board_full.check_board_full(board, "[ ]"):
+        print("\nIt's a draw!")
+    else:
+        next(player_loop)
+        print(f"\n{next(player_loop)['name']} wins!")
+
+
+def main():
+    """
+    Drive the program.
+    """
+    connect_four()
+
+
+if __name__ == "__main__":
+    main()
